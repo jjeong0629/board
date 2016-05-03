7111657587e2396af04eb44adb37f8e34a06d4e5
Choi <jjeong0629@gmail.com>
2016-05-03 07:49:32 +0000

    board

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,31 @@
+class HomeController < ApplicationController
+    def index
+    end
+    def write
+       
+        new_post = Post.new
+        new_post.title = params[:title]
+        new_post.content = params[:content]
+        new_post.save
+        
+        redirect_to "/list"
+    end
+    def list
+        @every_post = Post.all.order("id desc")
+    end
+    def destroy
+        @one_post = Post.find(params[:id])
+        @one_post.destroy
+        redirect_to "/list"
+    end
+    def update_view
+        @one_post = Post.find(params[:id])
+    end
+    def real
+        @one_post = Post.find(params[:id])
+        @one_post.title = params[:title]
+        @one_post.content = params[:content]
+        @one_post.save
+        redirect_to "/list"
+    end
+end
